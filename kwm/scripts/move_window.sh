ac1aaca0fb11d52be4c965682848516922c5fd4e
Enrico Ghirardi <i@choco.me>
2016-11-28 14:28:04 +0100

    kwm: simplify move window script

--- kwm/scripts/move_window.sh
+++ kwm/scripts/move_window.sh
@@ -4,18 +4,8 @@ NEW_WINDOW=$(kwmc query window focused "$DIR")
 SAME_PARENT=$(kwmc query window parent "$FOCUSED_WINDOW" "$NEW_WINDOW")
 if [ "$SAME_PARENT" = "true" ]
 then
-    $kwmc window -s "$DIR"
+    kwmc window -s "$DIR"
 else
-    SPAWN=$(kwmc query tiling spawn)
-    CHILD=$(kwmc query window child "$NEW_WINDOW")
-    if [ "$CHILD" = "left" ]
-    then
-        kwmc config spawn right
-    else
-        kwmc config spawn left
-    fi
-
     kwmc window -m "$DIR"
     kwmc window -f $FOCUSED_WINDOW
-    kwmc config spawn "$SPAWN"
 fi
